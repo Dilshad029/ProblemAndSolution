e1e8e27cae3b92cd6bab0681bbca7d9402e24719
dilshad029 <dilshad029@gmail.com>
2023-03-04 13:06:29 +0530

    Array - 1920 - Build Array from Permutation

---
+++ LeetCode/JavaCode/Array/src/org/easy/Build_Array_from_Permutation1920.java
@@ -0,0 +1,24 @@
+package org.easy;
+
+import java.util.Arrays;
+
+public class Build_Array_from_Permutation1920 {
+
+    public static void main(String[] args) {
+           int[] output = {0,2,1,5,3,4};
+           int[] input = {0,2,1,5,3,4};
+           if (Arrays.compare(output, input) == 0){
+               System.out.println("Accepted");
+           } else {
+               System.out.println("Wrong ANS");
+           }
+    }
+
+    public static  int[] buildArray(int[] nums) {
+        int [] ans = new int [ nums.length] ;
+        for (int i = 0 ; i< nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
